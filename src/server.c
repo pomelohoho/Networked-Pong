b80a8fcb8e9cc1ad44d6688e85587fc9cfc911ac
pomelohoho <52699926+pomelohoho@users.noreply.github.com>
2023-12-12 02:45:43 -0600

    Update server.c

--- src/server.c
+++ src/server.c
@@ -3,15 +3,15 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
-#include "scheduler.h"
-#include "util.h"
+#include <pthread.h>
 #include "socket.h"
 #include "message.h"
 
+#define MAX_CLIENTS 2
 #define MAX_HEIGHT 15
 #define MAX_WIDTH 40
-#define WINNING_SCORE 5  // The game ends when a player reaches this score
-#define PADDLE_SIZE 3  // Increase the paddle size
+#define WINNING_SCORE 5
+#define PADDLE_SIZE 3
 
 typedef struct {
     float paddle1_y, paddle2_y;
@@ -21,18 +21,66 @@ typedef struct {
 } GameState;
 
 GameState state;
+pthread_mutex_t state_mutex = PTHREAD_MUTEX_INITIALIZER;
+int client_sockets[MAX_CLIENTS] = {0};
 
 void init_game_state(GameState *state);
 void update_game_state(GameState *state);
-void render_game(const GameState *state);
-void game_loop();
+void serialize_game_state(const GameState *state, char *buffer);
 
-void serialize_game_state(const GameState *state, char *buffer) {
-    sprintf(buffer, "%f,%f,%f,%f,%d,%d",
-            state->paddle1_y, state->paddle2_y,
-            state->ball_x, state->ball_y,
-            state->score1, state->score2);
+void* handle_client(void* arg) {
+    int client_socket_fd = *((int*)arg);
+    free(arg);
+
+    pthread_mutex_lock(&state_mutex);
+    printf("Client connected! Socket FD: %d\n", client_socket_fd);
+
+    int clientIndex = -1;
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (client_sockets[i] == 0) {
+            client_sockets[i] = client_socket_fd;
+            clientIndex = i;
+            break;
+        }
+    }
+    printf("Client will control %s paddle.\n", clientIndex == 0 ? "left" : "right");
+    pthread_mutex_unlock(&state_mutex);
+
+    char buffer[1024];
+    while (1) {
+        char* message = receive_message(client_socket_fd);
+        if (message == NULL) break;
+        message[strcspn(message, "\n")] = 0;
+
+        pthread_mutex_lock(&state_mutex);
+        if (clientIndex == 0) { // Left paddle control
+            if (strcmp(message, "w") == 0) state.paddle1_y--;
+            if (strcmp(message, "s") == 0) state.paddle1_y++;
+        } else if (clientIndex == 1) { // Right paddle control
+            if (strcmp(message, "w") == 0) state.paddle2_y--;
+            if (strcmp(message, "s") == 0) state.paddle2_y++;
+        }
+        update_game_state(&state);
+        serialize_game_state(&state, buffer);
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (client_sockets[i] != 0) {
+                send_message(client_sockets[i], buffer);
+            }
+        }
+        pthread_mutex_unlock(&state_mutex);
+
+        free(message);
+    }
+
+    pthread_mutex_lock(&state_mutex);
+    printf("Client %d disconnected.\n", client_socket_fd);
+    client_sockets[clientIndex] = 0;
+    pthread_mutex_unlock(&state_mutex);
+    close(client_socket_fd);
+    return NULL;
 }
+
+
 int main() {
     unsigned short port = 0;
     int server_socket_fd = server_socket_open(&port);
@@ -41,70 +89,52 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_socket_fd, 1)) {
+    if (listen(server_socket_fd, MAX_CLIENTS)) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %u\n", port);
 
+    init_game_state(&state);
+
     while (1) {
-        int client_socket_fd = server_socket_accept(server_socket_fd);
-        if (client_socket_fd == -1) {
+        int* client_socket_fd = malloc(sizeof(int));
+        *client_socket_fd = server_socket_accept(server_socket_fd);
+        if (*client_socket_fd == -1) {
             perror("Accept failed");
+            free(client_socket_fd);
             continue;
         }
 
-        printf("Client connected!\n");
-             // Initialize game state
-        init_game_state(&state);
-        
-        while (1) {
-            update_game_state(&state);
-            // Serialize and send the game state back to the client
-            char buffer[1024]; // Define an adequate buffer size
-            serialize_game_state(&state, buffer);
-            // Debug message: print the serialized game state
-            printf("Serialized GameState: %s\n", buffer);
-            send_message(client_socket_fd, buffer);
-            char* message = receive_message(client_socket_fd);
-            if (message == NULL) break;
-            
-             // Trim newline character at the end of the message
-            message[strcspn(message, "\n")] = 0;
-
-            // Process message for paddle control
-            if (strcmp(message, "w") == 0) {
-                state.paddle1_y--;
-                printf("paddle pos: %f\n", state.paddle1_y);
-                printf("ball: %f\n", state.ball_x);
-
-            } else if (strcmp(message, "s") == 0) {
-                state.paddle1_y++;\
-                printf("paddle pos: %f\n", state.paddle1_y);
-                printf("ball: %f\n", state.ball_x);
-                
+        pthread_mutex_lock(&state_mutex);
+        int connected_clients = 0;
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (client_sockets[i] != 0) {
+                connected_clients++;
             }
-
-            if (state.score1 >= WINNING_SCORE || state.score2 >= WINNING_SCORE) {
-                break;  // End the game loop
-            }
-    
-            free(message);
         }
 
-        printf("Client disconnected.\n");
-        close(client_socket_fd);
+        if (connected_clients >= MAX_CLIENTS) {
+            printf("Maximum clients reached. Rejecting client %d.\n", *client_socket_fd);
+            send_message(*client_socket_fd, "Server is full. Connection rejected.");
+            close(*client_socket_fd);
+            free(client_socket_fd);
+        } else {
+            pthread_t thread_id;
+            if (pthread_create(&thread_id, NULL, handle_client, client_socket_fd) != 0) {
+                perror("Failed to create thread");
+                close(*client_socket_fd);
+                free(client_socket_fd);
+            }
+        }
+        pthread_mutex_unlock(&state_mutex);
     }
-    // Display the winner
-    if (state.score1 >= WINNING_SCORE) {
-        printf("Player 1 wins!\n");
-    } else {
-        printf("Player 2 wins!\n");
-    }
+
     close(server_socket_fd);
     return 0;
 }
+
 
 void init_game_state(GameState *state) {
     state->paddle1_y = MAX_HEIGHT / 2;
@@ -166,4 +196,11 @@ void update_game_state(GameState *state) {
         state->ball_x = MAX_WIDTH / 2;
         state->ball_y = MAX_HEIGHT / 2;
     }
+}
+
+void serialize_game_state(const GameState *state, char *buffer) {
+    sprintf(buffer, "%f,%f,%f,%f,%d,%d",
+            state->paddle1_y, state->paddle2_y,
+            state->ball_x, state->ball_y,
+            state->score1, state->score2);
 }
