d4ffdf431fe2a09045f1333337c7912b75f4a841
pomelohoho <52699926+pomelohoho@users.noreply.github.com>
2023-12-14 16:50:30 -0600

    Update game_state.c

--- src/game_state.c
+++ src/game_state.c
@@ -5,7 +5,7 @@
 #include "util.h"
 
 #define MAX_HEIGHT 15
-#define MAX_WIDTH 40
+#define MAX_WIDTH 70
 #define WINNING_SCORE 5  // The game ends when a player reaches this score
 #define PADDLE_SIZE 3  // Increase the paddle size
 
@@ -29,26 +29,29 @@ void update_game_state(GameState *state) {
     if (state->ball_y <= 0 || state->ball_y >= MAX_HEIGHT - 1) {
         state->ball_dy = -state->ball_dy;
     }
-
     // Boundary checks for paddles
     if (state->paddle1_y < 1) {
         state->paddle1_y = 1;
-    } else if (state->paddle1_y > MAX_HEIGHT - PADDLE_SIZE) {
-        state->paddle1_y = MAX_HEIGHT - PADDLE_SIZE;
+    } else if (state->paddle1_y > MAX_HEIGHT - 2) {
+        state->paddle1_y = MAX_HEIGHT - 2;
     }
 
     if (state->paddle2_y < 1) {
         state->paddle2_y = 1;
-    } else if (state->paddle2_y > MAX_HEIGHT - PADDLE_SIZE) {
-        state->paddle2_y = MAX_HEIGHT - PADDLE_SIZE;
+    } else if (state->paddle2_y > MAX_HEIGHT - 2) {
+        state->paddle2_y = MAX_HEIGHT - 2;
     }
 
-    // Check for collisions with paddles
+    // Check for collisions with left paddle
     for (int i = 0; i < PADDLE_SIZE; i++) {
         if (state->ball_x <= 2 && (int)state->ball_y == state->paddle1_y + i) {
             state->ball_dx = -state->ball_dx; // Reverse ball direction
             break;
         }
+    }
+
+    // Check for collisions with right paddle
+    for (int i = 0; i < PADDLE_SIZE; i++) {
         if (state->ball_x >= MAX_WIDTH - 3 && (int)state->ball_y == state->paddle2_y + i) {
             state->ball_dx = -state->ball_dx;
             break;
@@ -67,24 +70,38 @@ void update_game_state(GameState *state) {
     }
 }
 
-void render_game(const GameState *state) {
-    clear(); // Clear the screen
-
-    // Draw left paddle
-    for (int i = 0; i < PADDLE_SIZE; i++) {
-        mvprintw(state->paddle1_y + i, 1, "|");
-    }
-
-    // Draw right paddle
-    for (int i = 0; i < PADDLE_SIZE; i++) {
-        mvprintw(state->paddle2_y + i, MAX_WIDTH - 2, "|");
-    }
-
-    // Draw ball
-    mvprintw(state->ball_y, state->ball_x, "O");
-
-    // Draw scores
-    mvprintw(0, (MAX_WIDTH / 2) - 10, "Score: %d - %d", state->score1, state->score2);
-
-    refresh(); // Refresh the screen to show changes
+void serialize_game_state(const GameState *state, char *buffer) {
+    sprintf(buffer, "%f,%f,%f,%f,%d,%d",
+            state->paddle1_y, state->paddle2_y,
+            state->ball_x, state->ball_y,
+            state->score1, state->score2);
 }
+
+// void deserialize_game_state(const char *buffer, GameState *state) {
+//     sscanf(buffer, "%f,%f,%f,%f,%d,%d",
+//            &state->paddle1_y, &state->paddle2_y,
+//            &state->ball_x, &state->ball_y,
+//            &state->score1, &state->score2);
+// }
+
+// void render_game(const GameState *state) {
+//     clear(); // Clear the screen
+
+//     // Draw left paddle
+//     for (int i = 0; i < PADDLE_SIZE; i++) {
+//         mvprintw(state->paddle1_y + i, 1, "|");
+//     }
+
+//     // Draw right paddle
+//     for (int i = 0; i < PADDLE_SIZE; i++) {
+//         mvprintw(state->paddle2_y + i, MAX_WIDTH - 2, "|");
+//     }
+
+//     // Draw ball
+//     mvprintw(state->ball_y, state->ball_x, "O");
+
+//     // Draw scores
+//     mvprintw(0, (MAX_WIDTH / 2) - 10, "Score: %d - %d", state->score1, state->score2);
+
+//     refresh(); // Refresh the screen to show changes
+// }
